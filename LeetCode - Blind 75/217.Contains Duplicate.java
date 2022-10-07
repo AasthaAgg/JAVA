a714cdee968fe9060e1d6d6a44cef7bf2dfae3b4
Aastha Aggarwal <69754514+AasthaAgg@users.noreply.github.com>
2022-10-07 21:32:08 +0530

    Arrays

---
+++ LeetCode - Blind 75/217.Contains Duplicate.java
@@ -0,0 +1,16 @@
+/*   217.Contains Duplicates     */
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if(nums.length == 0){
+			return true;
+		}
+		Arrays.sort(nums);
+        for(int i=0; i<nums.length-1; i++){
+            if(nums[i] == nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+    }
+}
