4e693db4128c4309a8631d386b2168c9764ccf42
Aastha Aggarwal <69754514+AasthaAgg@users.noreply.github.com>
2022-10-18 21:26:15 +0530

    LeetCode - 217 (Arrays)

--- LeetCode - Blind 75/217.Contains Duplicate.java
+++ LeetCode - Blind 75/217.Contains Duplicate.java
@@ -2,13 +2,18 @@
 
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        if(nums.length == 0){
-			return true;
-		}
-		Arrays.sort(nums);
-        for(int i=0; i<nums.length-1; i++){
-            if(nums[i] == nums[i+1]){
+        // Arrays.sort(nums);
+        // for(int i=0; i<nums.length-1; i++){
+        //     if(nums[i] == nums[i+1]){
+        //         return true;
+        //     }
+        // }
+        HashSet<Integer> numset = new HashSet<>();
+        for(int num : nums){
+            if(numset.contains(num)){
                 return true;
+            }else{
+                numset.add(num);
             }
         }
         return false;
