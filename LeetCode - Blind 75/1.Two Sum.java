b0da743e4a52746a2db32d7385a7a6b5f32ce1d1
Aastha Aggarwal <69754514+AasthaAgg@users.noreply.github.com>
2022-10-18 21:11:58 +0530

    LeetCode - 1

--- LeetCode - Blind 75/1.Two Sum.java
+++ LeetCode - Blind 75/1.Two Sum.java
@@ -3,15 +3,24 @@
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i=0; i<nums.length-1; i++){
-            for(int j=i+1; j<nums.length; j++){
-                if(nums[i] + nums[j] == target){
-                    res[0] = i;
-                    res[1] = j;
-					return new []int {i, j}; 
-                }
-            }
-        }
+//         for(int i=0; i<nums.length-1; i++){
+//             for(int j=i+1; j<nums.length; j++){
+//                 if(nums[i] + nums[j] == target){
+//                     res[0] = i;
+//                     res[1] = j;
+// 		    return new []int {i, j}; 
+//                 }
+//             }
+//         }
+	    
+	    Map<Integer, Integer> hm = new HashMap<>();
+	    for(int i=0; i<nums.length; i++){
+		if(hm.containsKey(target - nums[i])){
+			return new int[]{i, hm.get(target - nums[i])};
+		}else{
+			hm.put(nums[i], i);
+		}
+	    }
         return null;
     }
 }
